22abf4c445b7d8e23984330561946afeb2a09bcf
Justin Neese <jneese@infoarmor.com>
2019-03-12 14:07:14 -0700

    stuff

---
+++ src/org/foo/Utils.groovy
@@ -0,0 +1,21 @@
+package org.foo
+
+class Utils1 {
+    def func1() {
+        println("this is func 1")
+    }
+
+    def func2() {
+        println("this is func 2")
+    }
+}
+
+class Utils2 {
+    def func1() {
+        println("this is func 3")
+    }
+
+    def func2() {
+        println("this is func 4")
+    }
+}
