0600ea962f60f9c7cc9bf328e0a096f5366d710c
Justin Neese <jneese@infoarmor.com>
2019-03-12 18:13:46 -0700

    stuff

--- dsl/utilities/MyUtilities.groovy
+++ dsl/utilities/MyUtilities.groovy
@@ -1,9 +1,34 @@
 package utilities
 
 class MyUtilities {
-    static void addMyFeature(def job) {
-        job.with {
-            description('Arbitrary feature')
-        }
-    }
+    def script = """
+        @Library(["SharedLib"])
+
+        def appSpec = [
+            appName: "stuff",
+            domain: "test.com",
+            envName: "dev",
+            appEnv: "dev",
+        ]
+
+        def deploymentSpec = [
+            kubeNamespace: "test-api-dev",
+            replicas: 1,
+            affinity: ["zone": "phx"],
+            deployment: "\${appSpec.appName}-\${appSpec.envName}",
+            tolerations: null,
+            containerPorts: [
+                [port: 8443, proto: "TCP"],
+                [port: 8444, proto: "TCP"]
+            ]
+        ]
+
+        testContainerDeploy(
+            deploymentSpec: deploymentSpec,
+            appSpec: appSpec,
+            template: "test/test-service.j2.yml",
+            gitUrl: "git@bitbucket.org:test/\${appSpec.appName}.git",
+            imageFromBuild: "test/kube-build/\${appSpec.appName}/containers"
+        )
+    """.stripIdent()
 }
